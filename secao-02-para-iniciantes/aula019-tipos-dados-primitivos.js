f322461dbacd8164e6df9ce865b8ee4ed3cf3401
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2024-12-21 11:10:11 -0300

    Seção 02 - JavaScript básico, 21/12/2024 (manhã)

---
+++ secao-02-para-iniciantes/aula019-tipos-dados-primitivos.js
@@ -0,0 +1,20 @@
+// - Strings
+const nome = 'Erick';
+const sobrenome = 'Ferreira';
+
+// - Number
+const valor1 = 10;
+const valor2 = 10.52;
+
+// - Undefined
+// Não aponta para local algum na memória
+let nomeAluno;
+
+// - Null
+// Não aponta para local algum na memória
+// Utilizada para anular o valor de uma variável
+let sobrenomeAluno = null;
+
+// - Booleano
+let aprovado = true;
+let deficiente = false;
